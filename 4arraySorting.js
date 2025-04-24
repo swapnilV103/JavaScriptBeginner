88f628d1a62e3f64ddd1da3e8224bd581576d75d
Swapnil Vidhate <swapnilvidhate@Swapnils-MacBook-Pro.local>
2025-04-24 12:28:25 +0530

    Swapnil | sorting of strings and arrays in java script

---
+++ 4arraySorting.js
@@ -0,0 +1,17 @@
+
+// sorting strings in java-script
+let alpa = ['b','d','c','a']
+let alpha = ["b","d","c","a"]
+
+alpa.sort()
+alpha.sort()
+
+console.log("alpa: "+alpa)
+console.log('alpha: '+alpha)
+
+//if you try to give numbers to sort it will consider it as strings
+let nums = [3,2,5,1]
+console.log(nums.sort())
+
+//failure scenario
+
