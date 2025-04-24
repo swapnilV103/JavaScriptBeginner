02613d9c4283797bb7ce122f08748e302fd3d27f
Swapnil Vidhate <swapnilvidhate@Swapnils-MacBook-Pro.local>
2025-04-24 12:17:13 +0530

    Swapnil | simple hello world

---
+++ 1helloWorld.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+// varaibles are loosly coupled
+
+var num = 9;
+var strName = "Swapnil"
+
+// along with var can also use let, const.  var and let are almost same but not all the way
+// we cannot re-declare variable in let but can do it using var. Re-assign is allowed in both.
+//const ! re-assign, re-declare
+let a = 5;
+console.log(typeof(a))
+
+console.log(num+"  "+strName);
