2c1f987600db3ae2501caba838e15351676ad0e2
Swapnil Vidhate <swapnilvidhate@Swapnils-MacBook-Pro.local>
2025-04-25 16:47:56 +0530

    Swapnil | changes

--- 4arraySorting.js
+++ 4arraySorting.js
@@ -6,12 +6,19 @@ let alpha = ["b","d","c","a"]
 alpa.sort()
 alpha.sort()
 
-console.log("alpa: "+alpa)
+console.log(alpa)
+alpa.reverse()
+console.log(alpa)
 console.log('alpha: '+alpha)
 
 //if you try to give numbers to sort it will consider it as strings
 let nums = [3,2,5,1]
 console.log(nums.sort())
 
-//failure scenario
+//failure scenario in numbers
+var num = [12,19,4,25,3]
+console.log(num.sort())
 
+//after adding the logic, basically a bubble sort logic
+console.log(num.sort((a,b)=>a-b)) //ascendnig order
+console.log(num.sort((a,b)=>b-a)) //decending order
